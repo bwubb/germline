3ec2af5396a689bcbfc67312ad6fcef434d2899e
Brad Wubbenhorst <bwubbenhorst@gmail.com>
2017-06-26 11:40:31 -0400

    update
    added intervals file

--- write.config-HaplotypeCaller.py
+++ write.config-HaplotypeCaller.py
@@ -6,9 +6,9 @@ import argparse
 import datetime
 
 class Interval(object):
-	def __init__(self):
-		self.file='/home/bwubb/BED/AtopicDermatitis_v1.intervals'#
-		self.key='AtopicDermatitis_v1'
+	def __init__(self,file,key):
+		self.file=file
+		self.key=key
 
 class Sample(object):
 	def __init__(self,name,bam):
@@ -27,9 +27,8 @@ def get_GATK(gatkv):
 	v={'3.4':'/home/bwubb/GenomeAnalysisTK-3.4-46','3.7':'/home/bwubb/software/GenomeAnalysisTK-3.7'}
 	return v[gatkv]
 
-def write_script(sample,bam_out,build,gatkv,mem):
-	key=os.path.dirname(os.path.getcwd())
-	out_gvcf=os.path.abspath('data/work/{0}/{0}.{1}.raw.all-sites.snps.indels.g.vcf'.format(sample.name,key))
+def write_script(sample,bam_out,build,gatkv,lib,mem):
+	out_gvcf=os.path.abspath('data/work/{0}/{0}.{1}.raw.snps.indels.g.vcf'.format(sample.name,lib.key))
 	with open(os.path.abspath('data/config/{0}/haplotype.sh'.format(sample.name)),'wb') as script:
 		script.write('#!/bin/bash\n\n')
 		script.write('DATE={0}\n'.format(datetime.date.today().strftime("%Y%m%d")))
@@ -45,8 +44,8 @@ def write_script(sample,bam_out,build,gatkv,mem):
 		script.write(' --dbsnp $dbsnp \\\n')
 		if bam_out:
 			script.write(' --bamOutput {0} \\\n'.format(os.path.abspath('data/final/{0}/{0}.gatk.bam'.format(sample.name))))
-		#if lib!=None:
-		#	script.write(' -L {0} \\\n'.format(lib.file))
+		if lib.file!=None:
+			script.write(' -L {0} \\\n'.format(lib.file))
 		script.write(' -o {0}\n\n'.format(out_gvcf))
 
 
@@ -61,7 +60,7 @@ def get_args():
 	'''Parse sys.argv'''
 	parser=argparse.ArgumentParser()
 	parser.add_argument('-i','--infile', help='')#def is_file or not?
-	parser.add_argument('--lib',default=None,help='Name associated with capture library.')
+	parser.add_argument('--lib',help='Name associated with capture library.')
 	parser.add_argument('--bam-out',action='store_true',default=False,help='Write bam of realignment')
 	parser.add_argument('--build',default='GRCh37',help='What Reference version would you like?')
 	parser.add_argument('--gatkv',default='3.4',help='If you want to change the version of GATK used.')
@@ -72,9 +71,14 @@ def get_args():
 def main(argv=None):
 	args=get_args()
 	if args.lib!=None:
-		lib=
+		#load intervals file
+		with open('intervals_available.txt','rb') as file:
+			intv=dict(line.rstrip().split(',') for line in file)
+		if args.lib not in intv:
+			raise
+		lib=Interval(intv[args.lib],args.lib)
 	else:
-		lib=None
+		lib=Interval(None,'all-sites')
 	with open(args.infile,'rb') as file:
 		samples=file.read().splitlines()
 	
