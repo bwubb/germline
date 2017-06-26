991f83543a7102abe304b42c45618370f551b72b
Brad Wubbenhorst <bwubbenhorst@gmail.com>
2017-06-26 09:26:20 -0400

    update

--- write.config-HaplotypeCaller.py
+++ write.config-HaplotypeCaller.py
@@ -27,12 +27,9 @@ def get_GATK(gatkv):
 	v={'3.4':'/home/bwubb/GenomeAnalysisTK-3.4-46','3.7':'/home/bwubb/software/GenomeAnalysisTK-3.7'}
 	return v[gatkv]
 
-def write_script(sample,bam_out,build,gatkv,lib,mem):
-	if lib:
-		key=lib.key
-	else:
-		key=os.path.dirname(os.path.getcwd())
-	out_gvcf=os.path.abspath('data/work/{0}/{0}.{1}.raw.snps.indels.g.vcf'.format(sample.name,key))
+def write_script(sample,bam_out,build,gatkv,mem):
+	key=os.path.dirname(os.path.getcwd())
+	out_gvcf=os.path.abspath('data/work/{0}/{0}.{1}.raw.all-sites.snps.indels.g.vcf'.format(sample.name,key))
 	with open(os.path.abspath('data/config/{0}/haplotype.sh'.format(sample.name)),'wb') as script:
 		script.write('#!/bin/bash\n\n')
 		script.write('DATE={0}\n'.format(datetime.date.today().strftime("%Y%m%d")))
@@ -48,8 +45,8 @@ def write_script(sample,bam_out,build,gatkv,lib,mem):
 		script.write(' --dbsnp $dbsnp \\\n')
 		if bam_out:
 			script.write(' --bamOutput {0} \\\n'.format(os.path.abspath('data/final/{0}/{0}.gatk.bam'.format(sample.name))))
-		if lib!=None:
-			script.write(' -L {0} \\\n'.format(lib.file))
+		#if lib!=None:
+		#	script.write(' -L {0} \\\n'.format(lib.file))
 		script.write(' -o {0}\n\n'.format(out_gvcf))
 
 
@@ -68,14 +65,14 @@ def get_args():
 	parser.add_argument('--bam-out',action='store_true',default=False,help='Write bam of realignment')
 	parser.add_argument('--build',default='GRCh37',help='What Reference version would you like?')
 	parser.add_argument('--gatkv',default='3.4',help='If you want to change the version of GATK used.')
-	parser.add_argument('-M','--mem',default=10,type=int,help='How much memory for the java stack. measure in g')
+	parser.add_argument('-M','--mem',default=10,type=int,help='How much memory for the java stack. Measured in g')
 	return parser.parse_args()
 
 
 def main(argv=None):
 	args=get_args()
 	if args.lib!=None:
-		lib=Interval()
+		lib=
 	else:
 		lib=None
 	with open(args.infile,'rb') as file:
@@ -85,16 +82,23 @@ def main(argv=None):
 	mkdir_p('data/config')
 	mkdir_p('data/final')
 	mkdir_p('data/work')
+	
 	for name in samples:
+		bam=False
 		#if args.bam_path:
 		if os.path.isfile('bam_input/final/{0}/{0}.ready.bam'.format(name)):#
+			bam=os.path.abspath('bam_input/final/{0}/{0}.ready.bam'.format(name))
+		elif os.path.isfile('bam_input/final/{0}/{0}-ready.bam'.format(name))
+			bam=os.path.abspath('bam_input/final/{0}/{0}-ready.bam'.format(name))
+		else:
+			print 'Could not locate', name
+		
+		if bam:
 			mkdir_p('data/config/{0}'.format(name))
 			mkdir_p('data/final/{0}'.format(name))
 			mkdir_p('data/work/{0}'.format(name))
-			sample=Sample(name,os.path.abspath('bam_input/final/{0}/{0}.ready.bam'.format(name)))
+			sample=Sample(name,bam)
 			write_script(sample,args.bam_out,args.build,args.gatkv,lib,args.mem)
-		else:
-			print 'Could not locate', name
 
 if __name__=='__main__':
 	main()
